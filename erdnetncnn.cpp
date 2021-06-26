76e6e65eb4bc0a14031540a1d97d159c3c6473d9
Ivan Mang <ivanmang1997@gmail.com>
2021-06-26 19:28:21 +0800

    Try again

--- erdnetncnn.cpp
+++ erdnetncnn.cpp
@@ -16,59 +16,59 @@
 #include <simpleocv.h>
 #include "erdnet.h"
 
-static int draw_fps(cv::Mat& rgba)
-{
-    // resolve moving average
-    float avg_fps = 0.f;
-    {
-        static double t0 = 0.f;
-        static float fps_history[10] = {0.f};
+// static int draw_fps(cv::Mat& rgba)
+// {
+//     // resolve moving average
+//     float avg_fps = 0.f;
+//     {
+//         static double t0 = 0.f;
+//         static float fps_history[10] = {0.f};
 
-        double t1 = ncnn::get_current_time();
-        if (t0 == 0.f)
-        {
-            t0 = t1;
-            return 0;
-        }
+//         double t1 = ncnn::get_current_time();
+//         if (t0 == 0.f)
+//         {
+//             t0 = t1;
+//             return 0;
+//         }
 
-        float fps = 1000.f / (t1 - t0);
-        t0 = t1;
+//         float fps = 1000.f / (t1 - t0);
+//         t0 = t1;
 
-        for (int i = 9; i >= 1; i--)
-        {
-            fps_history[i] = fps_history[i - 1];
-        }
-        fps_history[0] = fps;
+//         for (int i = 9; i >= 1; i--)
+//         {
+//             fps_history[i] = fps_history[i - 1];
+//         }
+//         fps_history[0] = fps;
 
-        if (fps_history[9] == 0.f)
-        {
-            return 0;
-        }
+//         if (fps_history[9] == 0.f)
+//         {
+//             return 0;
+//         }
 
-        for (int i = 0; i < 10; i++)
-        {
-            avg_fps += fps_history[i];
-        }
-        avg_fps /= 10.f;
-    }
+//         for (int i = 0; i < 10; i++)
+//         {
+//             avg_fps += fps_history[i];
+//         }
+//         avg_fps /= 10.f;
+//     }
 
-    char text[32];
-    sprintf(text, "FPS=%.2f", avg_fps);
+//     char text[32];
+//     sprintf(text, "FPS=%.2f", avg_fps);
 
-    int baseLine = 0;
-    cv::Size label_size = cv::getTextSize(text, cv::FONT_HERSHEY_SIMPLEX, 0.5, 1, &baseLine);
+//     int baseLine = 0;
+//     cv::Size label_size = cv::getTextSize(text, cv::FONT_HERSHEY_SIMPLEX, 0.5, 1, &baseLine);
 
-    int y = 0;
-    int x = rgba.cols - label_size.width;
+//     int y = 0;
+//     int x = rgba.cols - label_size.width;
 
-    cv::rectangle(rgba, cv::Rect(cv::Point(x, y), cv::Size(label_size.width, label_size.height + baseLine)),
-                    cv::Scalar(255, 255, 255, 255), -1);
+//     cv::rectangle(rgba, cv::Rect(cv::Point(x, y), cv::Size(label_size.width, label_size.height + baseLine)),
+//                     cv::Scalar(255, 255, 255, 255), -1);
 
-    cv::putText(rgba, text, cv::Point(x, y + label_size.height),
-                cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 0, 0, 255));
+//     cv::putText(rgba, text, cv::Point(x, y + label_size.height),
+//                 cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 0, 0, 255));
 
-    return 0;
-}
+//     return 0;
+// }
 
 static ERDNet* g_erdnet = 0;
 static cv::Mat bg_bgr;
